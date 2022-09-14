a41b5816487fb353dea832a26590af81a82d50c4
Felix <felix.mark@hotmail.com>
2022-09-14 18:58:31 +0200

    Made an even bigger mess regarding the decoder.

--- reader/main.py
+++ reader/main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
+import imutils
 
 img = cv2.imread('test_images/photo1.jpg')
-
-
-# converting image into grayscale image
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)      # img in hsv space
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
 
 # setting threshold of gray image
 _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
@@ -19,6 +20,7 @@ cv2.imshow('threshold', threshold)
 cv2.waitKey(0)
 
 possible_fgc_ements = []
+centroids_only = []
 
 for contour in contours[1:]:
   
@@ -33,17 +35,34 @@ for contour in contours[1:]:
         y = int(M['m01']/M['m00'])
   
     contour_sides = len(approx)
+    bounding_rect = cv2.minAreaRect(contour)
+    (x, y), (width, height), angle = bounding_rect
+    bounding_rect_size = width * height
 
     # putting shape name at center of each shape
-    if contour_sides > 6 and contour_sides < 22:
+    if contour_sides > 6 and contour_sides < 22 and bounding_rect_size > 300:
+        box = cv2.boxPoints(bounding_rect)
+        box = np.int0(box)
+        # cv2.drawContours(img,[box],0,(0,0,255),2)
+
         possible_fgc_ements.append(
             {"contour": contour, "x": x, "y": y}
         )
+        centroids_only.append((x, y))
         cv2.drawContours(img, [contour], 0, (0, 255, 0), 2)
         cv2.putText(
-            img, str(len(approx)), (x, y),
+            img, str(round(bounding_rect_size, 1)), (int(x), int(y)),
             cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2
         )
+
+x = [p[0] for p in centroids_only]
+y = [p[1] for p in centroids_only]
+centroid = (sum(x) / len(centroids_only), sum(y) / len(centroids_only))
+centroid = (int(centroid[0]), int(centroid[1]))
+cv2.putText(
+    img, "Center?", centroid,
+    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2
+)
 
 min_offset = None
 min_offset_pair = None
@@ -55,6 +74,7 @@ for a, contour_dict_1 in enumerate(possible_fgc_ements):
         offset_y = abs(contour_dict_1["y"] - contour_dict_2["y"])
         total_offset = offset_x + offset_y
         if min_offset is None or total_offset < min_offset:
+            min_offset = total_offset
             min_offset_pair = (contour_dict_1, contour_dict_2)
 
 cv2.drawContours(img, [min_offset_pair[0]["contour"]], 0, (0, 0, 255), 2)
